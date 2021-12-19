2ddec7c5632eb233ce591e23f3e473295e9ac392
YESSINE <prodtf@gmail.com>
2021-12-19 22:32:11 +0100

    .

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="m-6 space-y-3.5">
-<<<<<<< HEAD
         <div class="flex justify-between">
             <h1 class="font-extrabold text-red-500 text-3xl">Home</h1>
             <div class="flex-col flex items-center">
@@ -14,34 +13,6 @@
                 @click="logout"
             >Logout</button>
         </div>
-=======
-        <h1 class="font-extrabold text-red-500 text-3xl">Home</h1>
-        <p class="text-slate-300">Name : {{ name }}</p>
-        <input
-            v-model="newName"
-            type="text"
-            class="p-2 rounded focus:outline-none focus:ring-2 focus:ring-red-500 bg-stone-300"
-            placeholder="Name"
-        />
-        <button
-            @click="saveName"
-            class="p-2 ml-5 rounded text-white bg-indigo-600 hover:bg-indigo-700 bg-clip-padding"
-        >Submit</button>
-
-        <button
-            @click="$router.push('/about')"
-            class="p-2 block rounded text-white bg-red-500 hover:bg-red-500/80"
-        >About</button>
-
-        <button
-            @click="$router.push('/products')"
-            class="p-2 block rounded text-white bg-cyan-700 hover:bg-cyan-800"
-        >Products</button>
-        <button
-            @click="$router.push('/todos')"
-            class="p-2 block rounded text-white bg-orange-500 hover:bg-orange-600"
-        >Todos</button>
->>>>>>> 63bd4f5b655bd4e3e9eee93e97ff8bc969c4661c
     </div>
 </template> 
 
